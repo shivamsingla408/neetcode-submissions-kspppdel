514b8015a60fa1935eb4aef14fde221d6d8d712c
Shivam Singla <132809854+shivamsingla408@users.noreply.github.com>
2026-05-23 13:24:37 +0530

    Add: subtree-of-a-binary-tree - submission-2

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-2.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {  
+   TreeNode find(TreeNode root , TreeNode subroot){
+       if(root==null)return null;
+       if(subroot== null)return null;
+         if(root.val == subroot.val){
+            TreeNode temp = root;
+            return temp ;
+         }
+       TreeNode lefti =  find(root.left,subroot);
+       if(lefti!=null)return lefti;
+       TreeNode righti= find(root.right,subroot);
+       if(righti!=null)return righti;
+        return null;
+    }
+    boolean check(TreeNode temp,TreeNode subRoot){
+      if(temp==null && subRoot==null)return true;
+              if(temp==null || subRoot==null)return false;
+              if(temp.val!=subRoot.val)return false;
+              boolean lefti = check(temp.left,subRoot.left);
+              if(lefti == false)return false;
+              boolean righti = check(temp.right,subRoot.right);
+              if(righti==false)return false;
+              return true;
+    }
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+          if (root == null && subRoot == null) return true;
+          if (root == null || subRoot == null) return false;
+             if(check(root,subRoot))return true;
+         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+}
