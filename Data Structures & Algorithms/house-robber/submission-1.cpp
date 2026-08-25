94ba719d7f5d9ad4251117defd3d55ec3bbce364
Shivam Singla <132809854+shivamsingla408@users.noreply.github.com>
2026-08-25 21:16:41 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+vector<int> dp;
+    int rob(vector<int>& nums) {
+        dp.resize(nums.size(),-1);
+        return dfs(nums,0);
+    }
+    int dfs(vector<int>& nums,int i){
+       if(i>=nums.size())return 0;
+       if(dp[i]!=-1)return dp[i];
+
+       dp[i] =  max(dfs(nums,i+1),nums[i]+dfs(nums,i+2));
+       return dp[i];
+    }
+};
