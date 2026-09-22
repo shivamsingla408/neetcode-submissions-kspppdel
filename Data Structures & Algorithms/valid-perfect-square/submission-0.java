41bb41019d5df48b9c38c81b41c311089c95d28e
Shivam Singla <132809854+shivamsingla408@users.noreply.github.com>
2026-09-22 21:14:40 +0530

    Add: valid-perfect-square - submission-0

---
+++ Data Structures & Algorithms/valid-perfect-square/submission-0.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean isPerfectSquare(int num) {
+        long l = 1, r = num;
+
+        while (l <= r) {
+            long m = l + (r - l) / 2;
+            long sq = m * m;
+            if (sq > num) {
+                r = m - 1;
+            } else if (sq < num) {
+                l = m + 1;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
